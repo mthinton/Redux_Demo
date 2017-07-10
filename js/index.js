3e82514c5a93bed90950ebc4566c0b86dde3cb3c
mthintoncoding <mthinton@gmail.com>
2017-07-09 21:44:18 -0400

    Added first example of action and reducer

--- js/index.js
+++ js/index.js
@@ -1,2 +1,9 @@
 require('babel-polyfill');
 
+import * as actions from '../actions/index';
+import * as reducers from '../reducers/index';
+
+
+const newRepository = "The best repository";
+
+addRepository(newRepository);
